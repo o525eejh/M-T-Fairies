123a45d6452a43bd361725366f9d44311811ccff
o525eejh <iyoojin.kim@gmail.com>
2022-04-07 14:53:16 -0500

    Created variables and initialized servo and button pins

--- Yujin/Yujin.ino
+++ Yujin/Yujin.ino
@@ -1,6 +1,21 @@
+/*
+AET Hansel and Gretel Enclosure
+Team Name: M&T Fairies
+Team Members: Reece, Yujin, and Keith
+Description: button and servo program
+ */
+
+//Servo code library
+#include<Servo.h>;
+
+//VARIABLES
+Servo myServo;
+const int buttonPin =7; 
+
 void setup() {
   // put your setup code here, to run once:
-
+  myServo.attach(9) ; 
+  pinMode(buttonPin, INPUT); 
 }
 
 void loop() {
