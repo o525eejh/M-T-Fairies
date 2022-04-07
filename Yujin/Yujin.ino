6c2394f4f38c8c46c907a742a041a5490c39b9d3
rreece15 <reece.riherd@utexas.edu>
2022-04-07 14:52:03 -0500

    update done in class

--- Yujin/Yujin.ino
+++ Yujin/Yujin.ino
@@ -1,6 +1,21 @@
+/*
+AET Arduio P2
+Team Name: M&T Faeries
+Team Members: Keith, Reece, Yujin
+Description: Hansel and Gretel Interactive Piece
+*/
+
+//Servo Code library
+#include<Servo.h>
+
+//VARIABLES
+Servo myServo;
+const int buttonPin = 7;
+
 void setup() {
   // put your setup code here, to run once:
-
+  myServo.attach(9);
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
