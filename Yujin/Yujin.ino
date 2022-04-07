844672694b62520210fb141004e9457808eae284
o525eejh <iyoojin.kim@gmail.com>
2022-04-07 14:58:19 -0500

    if button pressed, swivel servo

--- Yujin/Yujin.ino
+++ Yujin/Yujin.ino
@@ -14,11 +14,16 @@ const int buttonPin =7;
 
 void setup() {
   // put your setup code here, to run once:
-  myServo.attach(9) ; 
+  myServo.attach(9); // using .attach to initialize servo to pin 9
   pinMode(buttonPin, INPUT); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  // if button is pressed, swivel servo 180 degrees
+  if(digitalRead(buttonPin) == HIGH){
+    myServo.write(180);
+  } else {
+    myServo.write(0);
+  }
 }
