03e3910c32832afa45521596298daea7ff6c3ae7
o525eejh <iyoojin.kim@gmail.com>
2022-04-19 00:26:33 -0500

    Update Yujin.ino
    rough draft for gretel and witch turnaround

--- Yujin/Yujin.ino
+++ Yujin/Yujin.ino
@@ -5,25 +5,65 @@ Team Members: Reece, Yujin, and Keith
 Description: button and servo program
  */
 
-//Servo code library
-#include<Servo.h>;
+#include <Servo.h>
+#define SW 7
 
-//VARIABLES
-Servo myServo;
-const int buttonPin =7; 
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+bool one_time_high=true;
+bool one_time_low=true;
 
 void setup() {
-  // put your setup code here, to run once:
-  myServo.attach(9); // using .attach to initialize servo to pin 9
-  pinMode(buttonPin, INPUT); 
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  Serial.begin(9600);
+  pinMode(SW, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // if button is pressed, swivel servo 180 degrees
-  if(digitalRead(buttonPin) == HIGH){
-    myServo.write(180);
-  } else {
-    myServo.write(0);
+  if(digitalRead(SW)==LOW)
+  {
+      Serial.print("sw:LOW");
+      Serial.print(",one_time_low=");
+      Serial.print(one_time_low,DEC);
+      Serial.print(",one_time_high=");
+      Serial.print(one_time_high,DEC);
+      
+      if(one_time_low)
+      {
+
+        for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+           myservo.write(pos);              // tell servo to go to position in variable 'pos'
+          delay(15);                       // waits 15ms for the servo to reach the position
+        }      
+        one_time_high = true;
+        one_time_low = false;
+      }
   }
+  else
+  {
+      Serial.print("sw:HIGH");    
+      Serial.print(",one_time_low=");
+      Serial.print(one_time_low,DEC);
+      Serial.print(",one_time_high=");
+      Serial.print(one_time_high,DEC);
+      if(one_time_high)
+      {
+        for (pos=0 ; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+          // in steps of 1 degree
+          myservo.write(pos);              // tell servo to go to position in variable 'pos'
+          delay(15);                       // waits 15ms for the servo to reach the position
+        }
+        one_time_high = false;
+        one_time_low = true;
+      }
+      //for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+      //  myservo.write(pos);              // tell servo to go to position in variable 'pos'
+      //  delay(15);                       // waits 15ms for the servo to reach the position
+      //}
+  }
+  Serial.print("\n");
+  delay(1); 
 }
