bd7a866f6a05fa041aa202872c877d35fbfd3a67
o525eejh <iyoojin.kim@gmail.com>
2022-04-19 10:08:09 -0500

    created switch led

---
+++ Yujin/Switch_led/Switch_led.ino
@@ -0,0 +1,15 @@
+int buttonPin = 8;
+int ledPin = 13;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+}
+
+void loop() {
+  if (digitalRead(buttonPin) == LOW) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+}
